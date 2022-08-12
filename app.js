cf6aa0d8cbbcc7af456a615d0dedb4e8c88815a9
mohammadfazeelDevopsSalsoft <salops@SALKHINB2965.salsoft.net>
2022-08-12 13:59:52 +0500

    testing

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res) {
-	  res.send("Hello World");
-	  res.send("test ci");
+	  res.send("Hello to first pipeline");
 });
 
 app.listen(4001);
